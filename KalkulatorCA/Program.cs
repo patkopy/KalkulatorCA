8c2aa6ca299a3479498421f4b705680a960a2e72
Patryk <patryk.kopyscinski@gmail.com>
2021-08-11 22:34:40 +0200

    add basic actions

--- KalkulatorCA/Program.cs
+++ KalkulatorCA/Program.cs
@@ -10,6 +10,58 @@ namespace KalkulatorCA
     {
         static void Main(string[] args)
         {
+            double a, b, wynik;
+            string operacja = "";
+
+            Console.WriteLine("-----Kalkulator-----");
+
+            while (operacja != "exit")
+            {
+                Console.WriteLine("Podaj pierwszą liczbę: ");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                while (operacja != "clear")
+                {
+                    Console.WriteLine("Podaj działanie(/,*,-,+), wyczyść za pomocą 'clear' lub wyjdź za pomocą 'exit': ");
+                    operacja = Console.ReadLine();
+
+                    if (operacja == "exit") Environment.Exit(-1);
+                    if (operacja == "clear")
+                    {
+                        operacja = "";
+                        break;
+                    }
+
+                    Console.WriteLine("Podaj kolejną liczbę: ");
+                    b = Convert.ToDouble(Console.ReadLine());
+
+                    if (operacja == "/")
+                    {
+                        wynik = a / b;
+                        Console.WriteLine("Wynik: " + wynik);
+                        a = wynik;
+                    }
+                    if (operacja == "*")
+                    {
+                        wynik = a * b;
+                        Console.WriteLine("Wynik: " + wynik);
+                        a = wynik;
+                    }
+                    if (operacja == "-")
+                    {
+                        wynik = a - b;
+                        Console.WriteLine("Wynik: " + wynik);
+                        a = wynik;
+                    }
+                    if (operacja == "+")
+                    {
+                        wynik = a + b;
+                        Console.WriteLine("Wynik: " + wynik);
+                        a = wynik;
+                    }
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
